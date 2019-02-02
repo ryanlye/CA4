8788e6d43114580756a0042aa6e0fdb91e127c7d
ryanlye <44422204+ryanlye@users.noreply.github.com>
2019-02-02 16:55:57 +0800

    Add files via upload

--- IS3102_Project-war/src/java/HelperClasses/ShoppingCartLineItem.java
+++ IS3102_Project-war/src/java/HelperClasses/ShoppingCartLineItem.java
@@ -8,16 +8,23 @@ public class ShoppingCartLineItem{
     private double price;
     private int quantity;
     private long countryID;
-
+    
+    //Done by Ryan Lye (p1638611)
+    
     public ShoppingCartLineItem() {
-        this.id="";
-        this.SKU = "";
-        this.name = "";
-        this.imageURL = "";
-        this.price = 0.0;
-        this.quantity = 0;
-        this.countryID = 0;
     }
+
+    public ShoppingCartLineItem(String id, String SKU, String name, String imageURL, double price, int quantity, Long countryID) {
+        this.id=id;
+        this.SKU = SKU;
+        this.name = name;
+        this.imageURL = imageURL;
+        this.price = price;
+        this.quantity = quantity;
+        this.countryID = countryID;
+    }
+    
+
 
     public String getId() {
         return id;
